d65bfe8e5c37d9664d1b9180b404a1fe15518029
AshStar <3493329946@qq.com>
2022-11-28 19:39:03 +0800

    1

---
+++ PostProcessingLearn/Assets/Scripts/GaussianBlur.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GaussianBlur : PostEffectsBase {
+
+	public Shader gaussianBlurShader;
+	private Material gaussianBlurMaterial = null;
+
+	public Material material {  
+		get {
+			gaussianBlurMaterial = CheckShaderAndCreateMaterial(gaussianBlurShader, gaussianBlurMaterial);
+			return gaussianBlurMaterial;
+		}  
+	}
+
+	// 高斯模糊运算次数
+	[Range(0, 4)]
+	public int iterations = 3;
+	
+	// 越大模糊程度越大
+	[Range(0.2f, 3.0f)]
+	public float blurSpread = 0.6f;
+
+	//缩放为 1/downSample倍
+	[Range(1, 8)]
+	public int downSample = 2;
+	
+	/// 1st edition: just apply blur
+//	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+//		if (material != null) {
+//			int rtW = src.width;
+//			int rtH = src.height;
+//			RenderTexture buffer = RenderTexture.GetTemporary(rtW, rtH, 0);
+//
+//			// Render the vertical pass
+//			Graphics.Blit(src, buffer, material, 0);
+//			// Render the horizontal pass
+//			Graphics.Blit(buffer, dest, material, 1);
+//
+//			RenderTexture.ReleaseTemporary(buffer);
+//		} else {
+//			Graphics.Blit(src, dest);
+//		}
+//	} 
+
+	/// 2nd edition: scale the render texture
+//	void OnRenderImage (RenderTexture src, RenderTexture dest) {
+//		if (material != null) {
+//			int rtW = src.width/downSample;
+//			int rtH = src.height/downSample;
+//			RenderTexture buffer = RenderTexture.GetTemporary(rtW, rtH, 0);
+//			buffer.filterMode = FilterMode.Bilinear;
+//
+//			// Render the vertical pass
+//			Graphics.Blit(src, buffer, material, 0);
+//			// Render the horizontal pass
+//			Graphics.Blit(buffer, dest, material, 1);
+//
+//			RenderTexture.ReleaseTemporary(buffer);
+//		} else {
+//			Graphics.Blit(src, dest);
+//		}
+//	}
+
+	/// 3rd edition: use iterations for larger blur
+	void OnRenderImage (RenderTexture src, RenderTexture dest) {
+		if (material != null) {
+			int rtW = src.width/downSample;		
+			int rtH = src.height/downSample;
+
+			RenderTexture buffer0 = RenderTexture.GetTemporary(rtW, rtH, 0);
+			buffer0.filterMode = FilterMode.Bilinear;
+
+			Graphics.Blit(src, buffer0);
+
+			for (int i = 0; i < iterations; i++) {
+				material.SetFloat("_BlurSize", 1.0f + i * blurSpread);	//卷积运算时采样的间隔
+
+				RenderTexture buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+				// Render the vertical pass
+				Graphics.Blit(buffer0, buffer1, material, 0);	//第四个参数,0代表调用第1个pass，1代变调用第2个pass
+
+				RenderTexture.ReleaseTemporary(buffer0);			//释放不再使用的buffer
+				buffer0 = buffer1;
+				buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);	//因为我们把buffer1的data传给了buffer0；所以要重新给buffer1分配一张纹理空间
+
+				// Render the horizontal pass
+				Graphics.Blit(buffer0, buffer1, material, 1);
+
+				RenderTexture.ReleaseTemporary(buffer0);
+				buffer0 = buffer1;
+			}
+
+			Graphics.Blit(buffer0, dest);
+			RenderTexture.ReleaseTemporary(buffer0);	//用完后要release掉
+		} else {
+			Graphics.Blit(src, dest);
+		}
+	}
+}
